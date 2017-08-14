ea2b73339c77d9c522738cce26fafc51e37cea4a
Chris Mazzochi <cmazzochi81@gmail.com>
2017-08-14 14:40:52 -0400

    more glory

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,35 +1,72 @@
 import React, {Component} from 'react'
 import {Stage} from 'react-konva'
-import {Board} from '../styled/Home'
+import {Board, Squares} from '../styled/Home'
 
 class Home extends Component {
 
 	state = {
-		rows:3
+		rows:3,
+		gameState: new Array(9).fill(false),
+		ownMark: 'X',
+		otherMark: 'O',
+		gameOver: false,
+		yourTurn: true,
+		winner: false, 
+		win: false
 	}
 
+	//A component lifecycle function that will execute every time it is mounted to the DOM
 	componentWillMount(){
 		let height = window.innerHeight
 		let width = window.innerWidth
 		let size = (height < width) ? height * .8 : width * .8
 		let rows = this.state.rows
 		let unit = size / rows
+		let coordinates = []
+		for(let y = 0; y < rows; y++){
+			for(let x = 0; x < rows; x++){
+				coordinates.push([x*unit, y*unit])
+			}
+		}
 
 		this.setState({
 			size,
 			rows,
-			unit
+			unit, 
+			coordinates
 		})
-		
 	}
+
+		move = (marker, index) => {
+			console.log('Move made', marker, index)
+
+		}
+
+		makeAiMove = () => {
+
+		}
+
+		turingTest = () => {
+
+		}
+
+		recordGame = () => {
+
+		}	
+	
 
 	render(){
 		let{
 			size,
 			unit,
-			rows
+			rows,
+			coordinates,
+			gameState, 
+			win, 
+			gameOver,
+			yourTurn,
+			ownMark
 		} = this.state
-
 
 		return(
 				<div>
@@ -45,7 +82,16 @@ class Home extends Component {
 							size={size}
 						/>
 						
-						{/*<Squares/>*/}
+						<Squares
+							unit={unit}
+							coordinates={coordinates}
+							gameState={gameState}
+							win={win}
+							gameOver={gameOver}
+							yourTurn={yourTurn}
+							ownMark={ownMark}
+							move={this.move}
+						/>
 
 					</Stage>
 
--- src/styled/Home.js
+++ src/styled/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Layer, Line} from 'react-konva'
+import {Layer, Line, Text} from 'react-konva'
 
 export const Board = ({unit,size,rows}) => {
 	let grid = []
@@ -33,3 +33,55 @@ export const Board = ({unit,size,rows}) => {
 		</Layer>
 		)
 }
+
+export const Squares = ({
+	unit,
+	coordinates,
+	gameState,
+	win,
+	gameOver,
+	yourTurn,
+	ownMark,
+	move
+}) => {
+	let squares = coordinates.map((position,index) => {
+			let makeMove = move
+			let mark = gameState[index]
+			let fill = 'black'
+	//if the current square is also listed in the win array
+			if (win && win.includes(index)){
+				fill = "lightgreen"
+			}
+			if (gameOver || !yourTurn || mark) {
+				makeMove = () => console.log('no!')
+			}
+			return (
+			<Text
+				key={index}
+				index={index}
+				x={position[0]}
+				y={position[1]}
+				fontSize={unit}
+				width={unit}
+				text={mark}
+				fill={fill}
+				fontFamily={'Helvetica'}
+				align={'center'}
+				onClick={(event)=>{
+					let index = event.target.index
+					makeMove(index, ownMark)
+				}}
+			/>
+
+			)
+	})
+	return (
+
+		<Layer>
+		{squares}
+		</Layer>
+
+	)
+}
+
+
